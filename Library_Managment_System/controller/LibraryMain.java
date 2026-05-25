26f542cf37baaa25900d6060f6a22de6000105fd
sean22-blip <iseandxd@gmail.com>
2026-05-25 15:51:15 +0700

    having a hard time going back to unfinsihed projects but i am here

--- Library_Managment_System/controller/LibraryMain.java
+++ Library_Managment_System/controller/LibraryMain.java
@@ -1,10 +1,8 @@
 package Library_Managment_System.controller;
 
-import Library_Managment_System.user.Manager;
-import Library_Managment_System.user.Staff;
-
 public class LibraryMain {
    public static void main(String args[]) {
        System.out.println("Create Staff: ");
    //  Staff staff1 = new Manager()
     }
+}
